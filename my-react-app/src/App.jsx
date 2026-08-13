3f0ad5999f90b0d0fb3d2858997e78d79646de18
Raj Singh <rajsinghrajpoot407@gmail.com>
2026-08-13 20:48:58 +0530

    Initial React project

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+// function App() {
+
+
+//   return (
+//     <>
+//     <p>heelo ji</p>
+//     <button> heelo ji my button</button>
+//     </>
+//   )
+// }
+
+// export default App
---
+++ my-react-app/src/New.jsx
@@ -0,0 +1,17 @@
+import "./Nya.css"
+function Raj(){
+
+    let button = ()=>{
+        alert("buttonclicked")
+    }
+return(
+<>
+<button onClick={button}>clicked me</button>
+    
+pwd
+
+</>
+)
+}
+
+export default Raj
